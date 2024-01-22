e63cbee7933e51d82b977c5500eb6a9fcb960924
Isaac Henry <isaachenry751@gmail.com>
2024-01-22 12:03:52 -0500

    commit

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+
+import movieReducer from './movieReducer';
+import favoritesReducer from './favoritesReducer'
+
+const rootReducer = combineReducers({ movieState:movieReducer, favoriteState:favoritesReducer });
+
+export default rootReducer;
---
+++ src/reducers/movieReducer.js
@@ -0,0 +1,23 @@
+import { ADD_MOVIE, DELETE_MOVIE } from '../actions/movieActions.js';
+import movies from './../data.js';
+
+const initialState = {
+    movies: movies,
+    appTitle: "IMDB Movie Database"
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case DELETE_MOVIE:
+            // alert(action.payload)
+            return {
+                ...state,
+                movies: state.movies.filter(item=>(action.payload !== item.id)),
+            
+            }
+        default:
+            return state;
+    }
+}
+
+export default reducer;
